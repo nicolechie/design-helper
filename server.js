984095c030d821bd4ca2c069219f56bb4dc04db2
Nicole <nicolechie@mac.com>
2016-08-02 00:59:28 +0000

    heroku changes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var app = express();
 
+//Set default NODE_ENV and MONGO LINK 
+if (!process.env.NODE_ENV) { 
+    process.env.NODE_ENV = 'development';
+    process.env.MONGODB_NW = 'mongodb://nicolechie:taioRu26@ds017584.mlab.com:17584/shopping-list';
+}
+
 
   app.use(express.static(__dirname + '/app'));
   // app.use(express.bodyParser());
@@ -49,8 +55,36 @@ io.on('connection', function (socket) {
   });
 });
 
-mongoose.connect(process.env.MONGODB_NW).then(function() {
+mongoose.connect(process.env.MONGODB_NW).then(function(err) {
+    if (err) {
+        console.log("error", err);
+    }
     server.listen(8080 || process.env.PORT);
+    console.log("Connected");
 });
+
+// CONNECTION EVENTS
+// When successfully connected
+// mongoose.connection.on('connected', function () {  
+//   console.log('Mongoose default connection open to ');
+// }); 
+
+// // If the connection throws an error
+// mongoose.connection.on('error',function (err) {  
+//   console.log('Mongoose default connection error: ' + err);
+// }); 
+
+// // When the connection is disconnected
+// mongoose.connection.on('disconnected', function () {  
+//   console.log('Mongoose default connection disconnected'); 
+// });
+
+// // If the Node process ends, close the Mongoose connection 
+// process.on('SIGINT', function() {  
+//   mongoose.connection.close(function () { 
+//     console.log('Mongoose default connection disconnected through app termination'); 
+//     process.exit(0); 
+//   }); 
+// });
 
 exports.app = app;
